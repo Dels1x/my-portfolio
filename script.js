ee29bcc7a04751b03d612b362638aa80be91690a
delsix <delsixreborn@gmail.com>
2023-05-30 19:04:25 +0300

    scrollToSection fixed to be more fitting

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function scrollToSection(sectionId) {
 	const yCoordinate = rect.top + window.pageYOffset;
   
 	window.scrollTo({
-  		top: yCoordinate - 100, // -75 for accounting nav height
+  		top: yCoordinate - 90, // -90 for accounting nav height
   		behavior: 'smooth' // You can use 'auto' or 'instant' for different scrolling behavior
 	});
 }
